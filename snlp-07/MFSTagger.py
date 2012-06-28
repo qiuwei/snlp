d24fad7e9c39c60f83198932c24666d149871078
weiqiu <qiuwei1987@gmail.com>
2012-06-28 10:14:27 +0200

    add most frequency tagger as baseline system

---
+++ snlp-07/MFSTagger.py
@@ -0,0 +1,31 @@
+'''
+Created on Jun 28, 2012
+
+@author: wqiu
+'''
+
+class MFSTagger(object):
+    '''
+    most frequent sense Tagger, will be used as baseline system
+    '''
+
+
+    def __init__(self, model):
+        '''
+        Constructor
+        '''
+        self.prior_sense_dist = model
+        
+    
+    def tag(self, sentence):
+        context = map(lambda x: self.snstmr.stem(x), sentence.lower().strip().split())
+        predict_sense = "init"
+        prob = float('-inf')
+        for candi_sense in self.prior_sense_dist.keys():
+            #print candi_sense
+            current_prob = self.prior_sense_dist[candi_sense]
+            if current_prob > prob:
+                predict_sense = candi_sense
+                #update the probability
+                prob = current_prob
+        return predict_sense
